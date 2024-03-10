57866a944b4cef0dd6e3f9709b6f4e5985e4a92c
Filip Pavlovic <filpavlovic@gmail.com>
2024-03-10 15:20:19 +0100

    queens attempt #1

--- s1/a_queens.cpp
+++ s1/a_queens.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <cassert>
+#include <iostream>
 
 /* V této úloze budete programovat řešení tzv. problému osmi
  * královen (osmi dam). Vaše řešení bude predikát, kterého vstupem
@@ -25,13 +26,44 @@
  * pouze bitové operace a zejména nepoužívá standardní kontejnery.
  * Řešení, které bude nevhodně používat kontejnery (spadá sem např.
  * jakékoliv použití ‹std::vector›) nemůže získat známku A. */
+const uint64_t ONE = 0b1lu;
 
-bool queens( std::uint64_t board );
+
+
+bool queens( std::uint64_t board ) {
+    uint8_t cols = 0, rows = 0;
+    uint64_t cross1 = 0, cross2 = 0;
+    for (uint64_t r = 0; r < 8; ++r) {
+        for (uint64_t c = 0; c < 8; ++c) {
+            if (!(board & (ONE << (8 * r + c))))
+                continue;
+            if (rows & (ONE << r) 
+            ||  cols & (ONE << c) 
+            ||  cross1 & (ONE << (c + r)) 
+            ||  cross2 & (ONE << (7 + r - c)))
+                return false;
+            rows    |= ONE << r;
+            cols    |= ONE << c;
+            cross1  |= ONE << (c + r);
+            cross2  |= ONE << (7 + r - c);
+
+        }
+    }
+    return true;
+}
 
 int main()
 {
-    assert( queens( 0 ) );
-    assert( !queens( 3 ) );
-
+    assert( queens( 0lu ) );
+    assert(queens(1));
+    assert(queens(2));
+    assert( !queens( 3lu ) );
+    assert(!queens(0x4080lu));
+    assert(!queens(0x201));
+    assert(!queens(0x8000000000000001));
+    assert(!queens(0x8001000000008001));
+    assert(queens(0x8008010420024010));
+    assert(queens(0x8004012002104008));
+    assert(!queens(0x8000800000000000));
     return 0;
 }
