bfa8b073baa221b3b460dda1013758d8d01ba9f8
Filip Pavlovic <filpavlovic@gmail.com>
2024-05-06 15:42:41 +0200

    feat(a): done

--- s3/a_machine.cpp
+++ s3/a_machine.cpp
@@ -1,4 +1,9 @@
 #include <cstdint>
+#include <cassert>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <functional>
 
 /* V této úloze budete programovat jednoduchý registrový stroj
  * (model počítače). Stroj bude mít libovolný počet celočíselných
@@ -44,21 +49,124 @@
  * případě úměrná součtu nejvyšší použité adresy a nejvyššího
  * použitého indexu registru. */
 
+struct machine;
 enum class opcode { mov, add, mul, jmp, load, stor, hlt };
+
+struct instruction {
+    virtual void operator()(machine &, int32_t, int32_t, int32_t) const = 0;
+};
+
+struct add_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} add;
+struct mov_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} mov;
+struct mul_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} mul;
+struct jmp_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} jmp;
+struct load_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} load;
+struct stor_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} stor;
+struct hlt_i : public instruction {
+    void operator()(machine&, int32_t, int32_t, int32_t) const override;
+} hlt;
+
+const std::unordered_map<int32_t, instruction *> insts {
+    {static_cast<int32_t>(opcode::add), &add}, 
+    {static_cast<int32_t>(opcode::mov), &mov},
+    {static_cast<int32_t>(opcode::hlt), &hlt}, 
+    {static_cast<int32_t>(opcode::jmp), &jmp},
+    {static_cast<int32_t>(opcode::mul), &mul}, 
+    {static_cast<int32_t>(opcode::load), &load},
+    {static_cast<int32_t>(opcode::stor), &stor}
+};
 
 struct machine
 {
+
+    std::map<int32_t, int32_t> ram;
+    std::map<int32_t, int32_t> registers;
+    int32_t cur_addr = 0;
     /* Čtení a zápis paměti po jednotlivých slovech. */
 
-    std::int32_t get( std::int32_t addr ) const;
-    void         set( std::int32_t addr, std::int32_t v );
+    std::int32_t get( std::int32_t addr ) const {
+        return ram.at(addr);
+    }
+    void set( std::int32_t addr, std::int32_t v ) {
+        ram[addr] = v;
+    }
 
     /* Spuštění programu, počínaje adresou nula. Vrátí hodnotu
      * uloženou v ‹reg_1› zadaném instrukcí ‹hlt›, která výpočet
      * ukončila. */
 
-    std::int32_t run();
+    std::int32_t run() {
+        while (cur_addr >= 0) {
+            int32_t opcode = ram[cur_addr];
+            int32_t im = ram[cur_addr + 4];
+            int32_t reg1 = ram[cur_addr + 8];
+            int32_t reg2 = ram[cur_addr + 12];
+            cur_addr += 16;
+            (*insts.at(opcode))(*this, im, reg1, reg2);
+        }
+        return registers[1];
+    };
 };
+
+void mov_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    if (reg2) {
+        m.registers[reg1] = m.registers[reg2];
+    } else {
+        m.registers[reg1] = im;
+    }
+};
+void add_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    if (reg2) {
+        m.registers[reg1] += m.registers[reg2];
+    } else {
+        m.registers[reg1] += im;
+    }
+};
+
+void jmp_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    (void) im;
+    if (!reg2 || m.registers[reg2]) 
+        m.cur_addr = m.registers[reg1];
+};
+
+void mul_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    if (reg2) {
+        m.registers[reg1] *= m.registers[reg2];
+    } else {
+        m.registers[reg1] *= im;
+    }
+};
+
+void load_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    (void) im;
+    m.registers[reg1] = m.ram[m.registers[reg2]];
+};
+
+void stor_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    (void) im;
+    m.ram[m.registers[reg2]] = m.registers[reg1];
+
+};
+
+void hlt_i::operator()(machine &m, int32_t im, int32_t reg1, int32_t reg2) const {
+    (void) im;
+    (void) reg1;
+    if (!reg2 || m.registers[reg2]) 
+        m.cur_addr = -1;
+};
+
 
 int main()
 {
