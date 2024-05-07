cc1d23913fd6eb88972107156141a56676aa1d36
Filip Pavlovic <filpavlovic@gmail.com>
2024-05-07 21:21:18 +0200

    feat(p1): done

--- 12/p1_set.cpp
+++ 12/p1_set.cpp
@@ -1,5 +1,11 @@
 #include <cassert>
+#include <cstdlib>
+#include <istream>
 #include <sstream>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
 /* Implementujte typ ‹set›, který reprezentuje množinu libovolných
  * celých čísel, s operacemi:
@@ -19,7 +25,52 @@
  * Na vstupu akceptujte i varianty s jiným počtem bílých znaků
  * (včetně žádných). */
 
-struct set;
+struct set {
+    std::set<int> in;
+
+    void add(int x) {
+        in.insert(x);
+    }
+
+    bool has(int x) const {
+        return in.contains(x);
+    }
+
+    std::size_t size() const {
+        return in.size();
+    }
+
+    friend std::istream &operator>>(std::istream &is, set &s) {
+        std::vector<std::string> set_str = {""};
+        s.in = {};
+        char c = 0;
+        while (c != '{')
+            is >> c;
+        is >> c;
+        while (c != '}') {
+            if (c == ',')
+                set_str.emplace_back();
+            else
+                set_str.back().push_back(c);
+            is >> c;
+        }
+        if (std::find_if(set_str.back().begin(), set_str.back().end(), [](char c){ return std::isdigit(c); }) == set_str.back().end())
+            set_str.pop_back();
+        for (const auto &str: set_str) {
+            s.add(std::atoi(str.data()));
+        }
+        return is;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const set &s) {
+        os << "{";
+        for (auto it = s.in.begin(); it != s.in.end(); ++it) {
+            os << " " << *it << ((++it == s.in.end()) ? " " : ",");
+            --it;
+        }
+        return os << "}";
+    }
+};
 
 int main()
 {
