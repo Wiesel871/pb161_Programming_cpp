9e50a9fa643cc1a7972998d21ce5e2a95bc26f92
Filip Pavlovic <filpavlovic@gmail.com>
2024-05-08 14:14:43 +0200

    feat(p6): done

--- 12/p6_csv.cpp
+++ 12/p6_csv.cpp
@@ -1,5 +1,11 @@
+#include <cctype>
+#include <cstddef>
+#include <cstdlib>
 #include <sstream>
 #include <cassert>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
 /* In this exercise, we will deal with CSV files: we will implement
  * a class called ‹csv› which will read data from an input stream
@@ -7,7 +13,7 @@
 
 /* The exception to throw in case of format error. */
 
-class bad_format;
+class bad_format {};
 
 /* The constructor should accept a reference to ‹std::istream› and
  * the expected number of columns. In the input, each line contains
@@ -16,8 +22,49 @@ class bad_format;
  *
  * Additionally, if ‹x› is an instance of ‹csv›, then ‹x.at( 3, 1 )›
  * should return the value in the third row and first column. */
+struct line : std::string { 
+    friend std::istream & operator>>(std::istream &is, line &line) {   
+        return std::getline(is, line);
+    }
+};
 
-class csv;
+class csv {
+    std::vector<std::vector<int>> data;
+
+    void append(auto st, auto end, int &cur_col) {
+        if (std::find_if(st, end, [](int c) {return !isdigit(c) && !isspace(c) && c != '\0';}) != end)
+            throw bad_format();
+        std::string col(st, end);
+        data.back().push_back(std::atoi(col.data()));
+        ++cur_col;
+    }
+
+    public:
+    csv(std::istream &is, int cols) {
+        line line;
+        while (is >> line) {
+            data.emplace_back();
+            int cur_col = 0;
+            auto st = line.begin();
+            auto prev = st;
+            while ((st = std::find(st, line.end(), ',')) != line.end()) {
+                if (std::find_if(prev, st, [](int c) {return !isdigit(c) && !isspace(c) && c != '\0';}) != st)
+                    throw bad_format();
+                append(prev, st, cur_col);
+                ++st;
+                prev = st;
+            }
+            append(prev, st, cur_col);
+
+            if (cols != cur_col)
+                throw bad_format();
+        }
+    }
+
+    int at(std::size_t i, std::size_t j) const {
+        return data.at(i).at(j);
+    }
+};
 
 int main()
 {
